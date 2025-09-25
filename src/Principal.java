f1769ac199ec0238216baa3b30b9753f5e0c62da
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2025-09-25 10:17:59 -0500

    Modifique los archivos colocando rojos

--- src/Principal.java
+++ src/Principal.java
@@ -5,5 +5,7 @@ public class Principal {
     public static void main(String[] args) {
         System.out.println("");
         UtilidadABinarios utilArchivos = new UtilidadABinarios();
+        slkdjfkdslkfsldk
+                sdñljflsdjfolps
     }
 }
--- src/pq1/Otra.java
+++ src/pq1/Otra.java
@@ -2,4 +2,7 @@ package pq1;
 
 public class Otra {
     private String a;
+    private Strong b;
+    lñlksñf dskjñlksd nlkfs
+
 }
