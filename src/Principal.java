73e6eb01d5b81e0d71be0b81070bf3fb2d82c7a0
Salas Didacticas Sede Bogota <salas@unilibre.edu.co>
2025-09-25 11:00:12 -0500

    archivos planos

--- src/Principal.java
+++ src/Principal.java
@@ -1,3 +1,4 @@
+import dev.vinni.util.UtilArchivos;
 import dev.vinni.util.UtilidadABinarios;
 import pq1.Otra;
 
@@ -17,5 +18,8 @@ public class Principal {
         Object dato = UtilidadABinarios.leer("archivo.txt1");
         Otra otra1 = (Otra)dato;
         System.out.println("Recuperado "+otra1);
+        String[] arreglo  = new String[1];
+        arreglo[0] = "Mi nombre es oasjd oas ";
+        UtilArchivos.guardar("uno",arreglo, false );
     }
 }
